c91f4b0803607c179827d5afb2d53fef363f57ca
jcm2512 <joe.magalotti@gmail.com>
2022-12-29 05:02:34 +0900

    init

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image
+        source={require("./assets/phonics_1_5.png")}
+        style={{ height: 1024, width: 724 }}
+      />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
